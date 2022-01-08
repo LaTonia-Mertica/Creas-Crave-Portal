8b9cffabc26629cd1a68c9d2b9782fa65fe84e1b
La'Tonia Mertica <latoniamertica@gmail.com>
2022-01-08 11:26:30 -0500

    debug complete, commented out login compoment import, final push

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
-import Login from "./Login";
+// import Login from "./Login";
 
 ReactDOM.render(
   <React.StrictMode>
