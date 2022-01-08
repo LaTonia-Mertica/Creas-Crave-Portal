17752361d78b072f8c6d17daf995455b2be62274
La'Tonia Mertica <latoniamertica@gmail.com>
2022-01-08 11:21:34 -0500

    defined auth0 for debug

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import Login from "./Login";
 
 ReactDOM.render(
